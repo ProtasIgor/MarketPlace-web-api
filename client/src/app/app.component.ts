776031d035ddb46168d1f5d27c08f49fe3dc450c
ProtasIgor <21it3.protas.i@pdu.by>
2023-11-29 17:29:10 +0300

    Intro HTTP client Module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MarketPlace';
+
+  products: any[];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5128/api/products').subscribe((responce: any) => {
+      this.products = responce.data
+    }, error => {
+      console.log(error)
+    })
+  }
 }
