0c942eedb072a8e9dca2d130659e4e72d1e9054c
Igor_Protas <21it3.protas.i@pdu.by>
2023-11-30 08:19:50 +0300

    Build UI Client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +8,8 @@ import { IPagination } from './models/pagination';
 export class AppComponent implements OnInit {
   title = 'MarketPlace';
 
-  products: IProduct[];
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor() {  }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5128/api/products').subscribe((responce: IPagination) => {
-      this.products = responce.data
-    }, error => {
-      console.log(error)
-    })
   }
 }
